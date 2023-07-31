585b9163d0d46fe37ba11d36b8823df1616d329e
Wolfite <100052562+WWolfite@users.noreply.github.com>
2023-07-31 22:40:16 +0100

    Update p2.php

--- p2.php
+++ p2.php
@@ -8,7 +8,6 @@
     <div id="data-container"></div>
 
     <script>
-
         // AJAX request to the PHP proxy script
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'p2.php?url=https://api.wolfite.net/404-quotes.json', true);
@@ -17,12 +16,17 @@
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
-                    // Successful response, process the JSON data
+                    // Successful response, parse the JSON data
                     var jsonData = JSON.parse(xhr.responseText);
 
                     // Display the JSON data on the web page
                     var dataContainer = document.getElementById('data-container');
-                    dataContainer.textContent = JSON.stringify(jsonData, null, 2); // Display formatted JSON
+                    // Convert the JSON object to a formatted string for display
+                    var formattedJsonData = JSON.stringify(jsonData, null, 2);
+                    // Convert any special characters (e.g., <, >, &) to their HTML entities to prevent XSS
+                    var escapedFormattedJsonData = document.createElement('div');
+                    escapedFormattedJsonData.textContent = formattedJsonData;
+                    dataContainer.appendChild(escapedFormattedJsonData);
                 } else {
                     // Handle errors if necessary
                     console.error('Error fetching data: ' + xhr.status);
